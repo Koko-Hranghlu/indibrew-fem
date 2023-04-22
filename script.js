85b4a63daca4cf8d2ca1dfe9ff812c250cfe798f
Hapari Paya <hranghlukoko@gmail.com>
2023-04-22 08:13:32 +0530

    first

---
+++ script.js
@@ -0,0 +1,3 @@
+const hamburger_icon = document.querySelector(".hamburger-icon")
+const nav_links = document.querySelector(".nav-links")
+hamburger_icon.addEventListener("click", () => nav_links.classList.toggle('active'))
